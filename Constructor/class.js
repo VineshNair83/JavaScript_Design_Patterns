ea8ddd60ab9194ea80f63bbed066ad5ccfb627b7
VineshNair83 <Vinesh@vineshs-mbp.lan>
2020-03-08 20:22:56 -0400

    JavaScript Design Patterns

---
+++ Constructor/class.js
@@ -0,0 +1,28 @@
+class Task {
+  constructor(name){
+    this.name = name;
+    this.completed = false;
+  }  
+
+  complete(){
+    console.log('completing task: ' + this.name);
+    this.completed = true; 
+  }
+  
+  save(){
+    console.log('saving Task: ' + this.name);  
+  }
+  
+}
+
+
+var task1 = new Task('Create a demo for constructors');
+var task2 = new Task('Create a demo for modules');
+var task3 = new Task('Create a demo for singletons');
+var task4 = new Task('Create a demo for prototypes');
+
+
+task1.complete();
+task2.save();
+task3.save();
+task4.save();
---
+++ README.md
@@ -0,0 +1,1 @@
+Java Script Pratical Design Patterns
